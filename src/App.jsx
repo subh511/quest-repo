4f6e21d16ce82cfb6a761880f44662df2999f5b8
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-03 20:43:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import Button from "./components/Button";
+//import UserDetail from "./components/Fetching";
+import Fetching from "./components/Fetching";
+
+function App() {
+  return (
+    <div>
+    <Fetching/>
+    </div>
+  );
+}
+
+//<Button/>
+
+export default App;
---
+++ src/components/Fetching.jsx
@@ -0,0 +1,75 @@
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// export default function Fetching() {
+
+//   const [users, setUsers] = useState([]);
+//   //const [isClicked,setIsClicked] = useState(false);
+
+//   useEffect(() => {
+//     const getUsers = async () => {
+//       let response = await axios.get(`https://reqres.in/api/users`);
+//       setUsers(response.data.data);
+//     };
+//     getUsers();
+//   }, []);
+
+// //   function smash(userId){
+// //     setIsClicked(userId)
+// //   }
+
+//   return (
+//     <div className="grid grid-cols-3">
+//       {users.map((user) => {
+//         return (
+//           <div key={user.id}>
+//             <ul>
+//               <li>
+//                 Name: {user.first_name} {user.last_name}
+//               </li>
+//               <li>Email: {user.email}</li>
+//             </ul>
+//             <div>
+//               <img src={user.avatar} alt={user.first_name} />
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// import { useState } from "react";
+// import axios from "axios";
+// import Button from "./Button";
+
+// function Fetching() {
+//   const [userData, setUserData] = useState(null);
+
+//   const fetchUserData = async (userId) => {
+//     try {
+//       const response = await axios.get(`https://reqres.in/api/users/${userId}`);
+//       setUserData(response.data.data); // Assuming data is wrapped inside 'data' property
+//     } catch (error) {
+//       console.error("Error fetching user data:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Button fetchUserData={fetchUserData} />
+//       {userData && (
+//         <div>
+//           <img src={userData.avatar} alt={userData.first_name} />
+//           <p>
+//             Name: {userData.first_name} {userData.last_name}
+//           </p>
+//           <p>Email: {userData.email}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Fetching;
+
+
