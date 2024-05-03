39f3f6bf983d114c2825c13a34de5898c2ba8210
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-03 22:01:08 +0530

    modified, done alert as per said, final push

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({fetchData}) {
     <button className="px-4 rounded-md p-4 mx-4 mt-4 bg-slate-300 font-bold" onClick={() => fetchData(2)}>User 2</button>
     <button className="px-4 rounded-md p-4 mx-4 mt-4 bg-slate-300 font-bold" onClick={() => fetchData(3)}>User 3</button>
     <button className="px-4 rounded-md p-4 mx-4 mt-4 bg-slate-300 font-bold" onClick={() => fetchData(4)}>User 4</button>
-    <button className="px-4 rounded-md p-4 mx-4 mt-4 bg-slate-300 font-bold" onClick={() => fetchData(5)}>User 5</button>
+    <button className="px-4 rounded-md p-4 mx-4 mt-4 bg-slate-300 font-bold" onClick={() => alert("No Data Found!")}>User 5</button>
     </div>
   )
 }
